659c9823a7032a5b7268c914e67bd8d5add3d569
Billy Vong <billy@opstarts.com>
2016-12-30 15:36:06 -0500

    ignore for eslint/flow for now

--- rplugin/node/jsimport.js/src/getImportString.js
+++ rplugin/node/jsimport.js/src/getImportString.js
@@ -1,11 +1,12 @@
-/* @flow */
+// @flow
+
 type Params = {
   word: string,
   importPath: string,
-  context: ContextT,
+  context: any,
 };
 
-export default function getImportString(nvim: Nvim, { /*buffer, */word, importPath, context }: Params) {
+export default function getImportString(nvim: any, { /*buffer, */word, importPath, context }: Params) {
   if (context.type === 'named') {
     return [`import { ${word} } from '${importPath}';`];
   }
