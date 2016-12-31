6ddc37434ce97c422e7e71acd1e087228d675451
Billy Vong <billy@opstarts.com>
2016-12-31 11:02:24 -0500

    Add more test files for `gatherExports`

--- rplugin/node/jsimport.js/src/__tests__/gatherExports.test.js
+++ rplugin/node/jsimport.js/src/__tests__/gatherExports.test.js
@@ -2,13 +2,42 @@ import gatherExports from '../gatherExports';
 import path from 'path';
 
 const GATHER_DIR = path.resolve(__dirname, '../../test_resources/gather_test/');
+const REL = '../../../../..';
+
 describe('gatherExports', function() {
-  it('can gather exports with default settings', async function() {
+  it('gathers ".js" and ".jsx" files by default', async function() {
     const files = await gatherExports(GATHER_DIR);
 
     // We can't save the entire file path because snapshots will fail in other envs
     expect(
-      files.map((file) => file.replace(path.resolve(__dirname, '../../../../..'), ''))
+      files.map((file) => file.replace(path.resolve(__dirname, REL), ''))
+    ).toMatchSnapshot();
+  });
+
+  it('only gathers from ".js" files', async function() {
+    const files = await gatherExports(GATHER_DIR, { pattern: '\.js$' });
+
+    // We can't save the entire file path because snapshots will fail in other envs
+    expect(
+      files.map((file) => file.replace(path.resolve(__dirname, REL), ''))
+    ).toMatchSnapshot();
+  });
+
+  it('ignores "child/react" full file path pattern', async function() {
+    const files = await gatherExports(GATHER_DIR, { ignore: ['child/react']});
+
+    // We can't save the entire file path because snapshots will fail in other envs
+    expect(
+      files.map((file) => file.replace(path.resolve(__dirname, REL), ''))
+    ).toMatchSnapshot();
+  });
+
+  it('ignores ".jsx" files', async function() {
+    const files = await gatherExports(GATHER_DIR, { ignore: ['\.jsx$']});
+
+    // We can't save the entire file path because snapshots will fail in other envs
+    expect(
+      files.map((file) => file.replace(path.resolve(__dirname, REL), ''))
     ).toMatchSnapshot();
   });
 });
---
+++ rplugin/node/jsimport.js/test_resources/gather_test/reactComponent.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class DefaultComponent extends Component {
+  render() {
+    return (
+      <div />
+    );
+  }
+}
